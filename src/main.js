353a30ffc99ff34c19288296fcb4745630fa55ae
Андрей <cereperec@gmail.com>
2020-08-02 17:04:29 +0300

    произведена первичная настройка webpack

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-"use strict";
+import {createMenuTemplate} from "./view/site-menu.js";
 
 const createUserRankTemplate = () => {
   return (
@@ -6,21 +6,6 @@ const createUserRankTemplate = () => {
     <p class="profile__rating">Movie Buff</p>
     <img class="profile__avatar" src="images/bitmap@2x.png" alt="Avatar" width="35" height="35">
   </section>`
-  );
-};
-
-const createMenuTemplate = () => {
-  return (
-    `<nav class="main-navigation">
-    <div class="main-navigation__items">
-      <a href="#all" class="main-navigation__item main-navigation__item--active">All movies</a>
-      <a href="#watchlist" class="main-navigation__item">Watchlist <span class="main-navigation__item-count">13</span></a>
-      <a href="#history" class="main-navigation__item">History <span class="main-navigation__item-count">4</span></a>
-      <a href="#favorites" class="main-navigation__item">Favorites <span class="main-navigation__item-count">8</span></a>
-    </div>
-    <a href="#stats" class="main-navigation__additional">Stats</a>
-  </nav>
-`
   );
 };
 
