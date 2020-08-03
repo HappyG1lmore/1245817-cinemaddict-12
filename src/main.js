0811ff59f07904505202cc8b18bc57fb6bae9954
Андрей <cereperec@gmail.com>
2020-08-03 18:54:31 +0300

    Созданы и экспортированы отдельные модули

--- src/main.js
+++ src/main.js
@@ -1,80 +1,11 @@
 import {createMenuTemplate} from "./view/site-menu.js";
-
-const createUserRankTemplate = () => {
-  return (
-    `<section class="header__profile profile">
-    <p class="profile__rating">Movie Buff</p>
-    <img class="profile__avatar" src="images/bitmap@2x.png" alt="Avatar" width="35" height="35">
-  </section>`
-  );
-};
-
-const createSortingTemplate = () => {
-  return (
-    `<ul class="sort">
-    <li><a href="#" class="sort__button sort__button--active">Sort by default</a></li>
-    <li><a href="#" class="sort__button">Sort by date</a></li>
-    <li><a href="#" class="sort__button">Sort by rating</a></li>
-  </ul>`
-  );
-};
-
-const createFilmsContainer = () => {
-  return (
-    `<section class="films">
-      <section class="films-list">
-        <h2 class="films-list__title visually-hidden">All movies. Upcoming</h2>
-        <div class="films-list__container"></div>
-      </section>
-    </section>`
-  );
-};
-
-const createFilmCardTemplate = () => {
-  return (
-    `<article class="film-card">
-    <h3 class="film-card__title">The Man with the Golden Arm</h3>
-    <p class="film-card__rating">9.0</p>
-    <p class="film-card__info">
-      <span class="film-card__year">1955</span>
-      <span class="film-card__duration">1h 59m</span>
-      <span class="film-card__genre">Drama</span>
-    </p>
-    <img src="./images/posters/the-man-with-the-golden-arm.jpg" alt="" class="film-card__poster">
-    <p class="film-card__description">Frankie Machine (Frank Sinatra) is released from the federal Narcotic Farm in Lexington, Kentucky with a set of drums and a new outlook on…</p>
-    <a class="film-card__comments">18 comments</a>
-    <form class="film-card__controls">
-      <button class="film-card__controls-item button film-card__controls-item--add-to-watchlist">Add to watchlist</button>
-      <button class="film-card__controls-item button film-card__controls-item--mark-as-watched  film-card__controls-item--active">Mark as watched</button>
-      <button class="film-card__controls-item button film-card__controls-item--favorite">Mark as favorite</button>
-    </form>
-  </article>`
-  );
-};
-
-const createShowMoreButtonTemplate = () => {
-  return (
-    `<button class="films-list__show-more">Show more</button>`
-  );
-};
-
-const createTopTemplate = () => {
-  return (
-    `<section class="films-list--extra">
-    <h2 class="films-list__title">Title</h2>
-    <div class="films-list__container">
-    </div>
-  </section>`
-  );
-};
-
-const createFooterStatisticsTemplate = () => {
-  return (
-    `<section class="footer__statistics">
-    <p>130 291 movies inside</p>
-  </section>`
-  );
-};
+import {createUserRankTemplate} from "./view/site-user-rank.js";
+import {createSortingTemplate} from "./view/site-sorting.js";
+import {createShowMoreButtonTemplate} from "./view/site-show-more-button.js";
+import {createFilmCardTemplate} from "./view/site-film-card.js";
+import {createTopTemplate} from "./view/site-additional-section.js";
+import {createFilmsContainer} from "./view/site-films-container.js";
+import {createFooterStatisticsTemplate} from "./view/site-footer-statistics.js";
 
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
